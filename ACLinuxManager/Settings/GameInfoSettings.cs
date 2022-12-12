5d42f8e2c8780031578de19181719780754c7db7
Michal Tomecki <minexplaypl@gmail.com>
2022-12-12 20:52:24 +0100

    Changed auto theming from submodule to direct reference because of constant development

--- ACLinuxManager/Settings/GameInfoSettings.cs
+++ ACLinuxManager/Settings/GameInfoSettings.cs
@@ -2,5 +2,6 @@ namespace ACLinuxManager.Settings;
 
 public static class GameInfoSettings
 {
-
+    public static string GameDocumentsPath =
+        "/home/minik/.steam/steam/steamapps/compatdata/244210/pfx/drive_c/users/steamuser/Documents/Assetto Corsa/";
 }
